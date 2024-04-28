9b69eb5f61430dfdc6a7f73415e1f862b8fb76fc
PokeMasterLEE <iyeojae@gmail.com>
2024-04-28 23:53:04 +0900

    1-5 개인과제 /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
    /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
    
    /* 연산의 결과를 배열에 저장합니다. */
    
    /* index를 증가 시킵니다. */

--- src/App.java
+++ src/App.java
@@ -4,11 +4,15 @@ public class App {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
 
+        int [] resultlist = new int[10];
+
+        int lastindex = 0;
+
         while (true) {
             System.out.print("첫번째 숫자를 입력하세요 : ");
-            int x = a.nextInt();
+            int firstnum = a.nextInt();
             System.out.print("두번째 숫자를 입력하세요 : ");
-            int y = a.nextInt();
+            int secondnum = a.nextInt();
             a.nextLine();
             System.out.print("사칙연산 기호를 입력하세요 : ");
             String z = a.next();
@@ -16,26 +20,31 @@ public class App {
             int result = 0;
             switch (z) {
                 case "+" :
-                    result = x + y;
+                    result = firstnum + secondnum;
                     break;
                 case "-" :
-                    result = x - y;
+                    result = firstnum - secondnum;
                     break;
                 case "*" :
-                    result = x * y;
+                    result = firstnum * secondnum;
                     break;
                 case "/" :
-                    if(y == 0) {
+                    if(secondnum == 0) {
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                         break;
                     }
-                    result = x / y;
+                    result = firstnum / secondnum;
                     break;
                 default:
                     System.out.println("제대로 입력하세요...");
                     break;
             }
             System.out.println("결과 : " + result);
+
+            resultlist[lastindex] = result;
+            ++lastindex;
+            int remain = 10 - lastindex;
+            System.out.print("저장 공간이 " + remain + " 남았습니다.");
 
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)");
             String more = a.next();
