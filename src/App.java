d1e5aa26db2550eaaa5322615c4186d34007b5ed
PokeMasterLEE <iyeojae@gmail.com>
2024-04-28 23:18:36 +0900

    1-3 개인과제 /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/

--- src/App.java
+++ src/App.java
@@ -11,5 +11,29 @@ public class App {
         a.nextLine();
         System.out.print("사칙연산 기호를 입력하세요 : ");
         String z = a.next();
+
+        int result = 0;
+        switch (z) {
+            case "+" :
+                result = x + y;
+                break;
+            case "-" :
+                result = x - y;
+                break;
+            case "*" :
+                result = x * y;
+                break;
+            case "/" :
+                if(y == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = x / y;
+                break;
+            default:
+                System.out.println("제대로 입력하세요...");
+                break;
+        }
+        System.out.println("결과: " + result);
     }
 }
