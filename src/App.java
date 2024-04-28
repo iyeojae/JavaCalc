14977e4009b45d693c14da46e66877c637df2101
PokeMasterLEE <iyeojae@gmail.com>
2024-04-28 22:41:43 +0900

    1-1 개인과제

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+
+        System.out.print("첫번째 숫자를 입력하세요 : ");
+        int x = a.nextInt();
+        System.out.print("두번째 숫자를 입력하세요 : ");
+        int y = a.nextInt();
+    }
+}
