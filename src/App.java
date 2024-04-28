5bc36f4fa9da9111f3ca439b8c2f91c0596e225b
PokeMasterLEE <iyeojae@gmail.com>
2024-04-28 22:56:30 +0900

    1-2 개인과제

--- src/App.java
+++ src/App.java
@@ -8,5 +8,8 @@ public class App {
         int x = a.nextInt();
         System.out.print("두번째 숫자를 입력하세요 : ");
         int y = a.nextInt();
+        a.nextLine();
+        System.out.print("사칙연산 기호를 입력하세요 : ");
+        String z = a.next();
     }
 }
